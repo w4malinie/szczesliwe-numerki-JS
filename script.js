1b34e55cfc93b7069c56df661cb338c296fe864f
w4malinie <w4malinie@gmail.com>
2022-11-29 15:42:25 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const btn = document.getElementById("btn");
+const elH1 = document.querySelector("h1");
+const balls = document.querySelectorAll(".ball");
+
+const MIN_RESULT = 1;
+const MAX_RESULT = 49;
+
+function getRandomNum(min, max) {
+  min = Math.round(min);
+  max = Math.round(max);
+  return Math.round(Math.random() * (max - min));
+}
+
+const toggleSpinner = () => {
+  const spinner = document.getElementById("spinner-container");
+  spinner.classList.toggle("hidden");
+};
+
+function setActiveBalls(numbers) {
+  balls.forEach((ball, index) => {
+    ball.classList.add("active");
+    ball.innerText = numbers[index];
+  });
+}
+
+function generateNums() {
+  const results = [];
+
+  while (results.length <= 6) {
+    const randomNumber = getRandomNum(MIN_RESULT, MAX_RESULT);
+
+    if (!results.includes(randomNumber)) {
+      results.push(randomNumber);
+    }
+  }
+
+  return results;
+}
+
+function clearActiveBalls() {
+  balls.forEach((ball) => {
+    ball.classList.remove("active");
+  });
+}
+
+btn.addEventListener("click", () => {
+  toggleSpinner();
+  clearActiveBalls();
+  const numbers = generateNums();
+
+  elH1.innerText = "Twoje szczęśliwe numery, to:";
+  btn.innerText = "Losuj ponownie";
+  setTimeout(function () {
+    toggleSpinner();
+    setActiveBalls(numbers);
+  }, 1500);
+});
+
+// NOWE FUNKCJE:
+// 1. zmienne połączone 3 inputami: liczba numerków, zakres min  i max
+// 2. liczba numerów generuje ilość kulk (div ball)
+// 3. inputy w tekscie jako ___
